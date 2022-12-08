6a525158752d3ea1da6c474f99992f375f6dabaf
Flo <92175215+mopsfl@users.noreply.github.com>
2022-12-08 18:29:03 +0100

    Update scprp_getweapondata.lua

--- other/scprp_getweapondata.lua
+++ other/scprp_getweapondata.lua
@@ -1,5 +1,8 @@
 --[[
 This gets the current Weapon Data of the guns in your inventory
+you must have your gun equiped atleast once to get data
+
+
 Im trying to make an Infinite Ammo but, yeah idk, game very weird xd
 ]]
 
