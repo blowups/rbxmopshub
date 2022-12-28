d74e53e2fba3989725e3542906edd64ea4a9923a
Flo <92175215+mopsfl@users.noreply.github.com>
2022-12-28 03:21:43 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/mopsfl/rbxmopshub/edit/main/scriptloader.lua")()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mopsfl/rbxmopshub/main/scriptloader.lua")()
